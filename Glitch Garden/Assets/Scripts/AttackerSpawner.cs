65d481ff51e2dfff200b70ceb3c9850c6ee30a5d
Alexander Aguilar <alexanderman2004@yahoo.com>
2019-04-03 21:22:12 -0400

    first commit

---
+++ Glitch Garden/Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour {
+  [SerializeField] float minSpawnDelay = 1f;
+  [SerializeField] float maxSpawnDelay = 5f;
+  [SerializeField] Attacker attackPrefab;
+
+  bool spawn = true;
+
+  IEnumerator Start() {
+    while (spawn) {
+      yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+      SpawnAttacker();
+    }
+  }
+
+  private void SpawnAttacker() {
+    Instantiate(attackPrefab, transform.position, transform.rotation);
+  }
+}
---
+++ Glitch Garden/Assets/Scripts/LevelLoad.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad : MonoBehaviour {
+  [SerializeField] float waitInSeconds = 4f;
+
+  int currentSceneIndex;
+
+  private void Start() {
+    currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+    if (currentSceneIndex == 0) {
+      StartCoroutine(WaitForTime());
+    }
+  }
+
+  IEnumerator WaitForTime() {
+    yield return new WaitForSeconds(waitInSeconds);
+    LoadNextScene();
+  }
+
+  public void LoadNextScene() {
+    SceneManager.LoadScene(currentSceneIndex + 1);
+  }
+}
